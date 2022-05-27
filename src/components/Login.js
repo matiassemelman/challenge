a6699839535589d441ae585c1d7f629310aa5f4a
matiassemelman <matias.semelman@protonmail.com>
2022-05-27 12:52:28 -0300

    Feat: Created Login component + credential validation

---
+++ src/components/Login.js
@@ -0,0 +1,55 @@
+function Login() {
+
+    const submitHandler = e => {
+        e.preventDefault();
+
+        // Email declaration AND validation
+        const email = e.target.email.value;
+
+        const regexEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+        // Password declaration
+        const password = e.target.password.value
+
+
+        // Submit Handler Function
+
+        if ( email === '' || password === '') {
+            console.log('Fields cannot be empty');
+            return;
+        }
+        if (email !== '' && !regexEmail.test(email))  {
+            console.log('Email not valid')
+            return;
+        }
+
+        if ( email !== 'challenge@alkemy.org' || password !== 'react' ) {
+            console.log('Please enter a valid account')
+            return;
+        }
+        console.log('Ready to send info')
+    }
+
+
+    return (
+        <>
+        
+        <form onSubmit={submitHandler}>
+    <label htmlFor="email">Email
+        <br/>
+        <input type="email" name="email" id="email"/>
+    </label>
+    <br/>
+    <label htmlFor="password">Password
+        <br/>
+        <input type="password" name="password" id="password"/>
+    </label>
+    <br />
+    <button type="submit">Submit</button>
+</form>
+        </>
+    )
+}
+
+
+export default Login;
