985f746ae56e7cd044fc4f9a7e1c58961a065d4e
matiassemelman <matias.semelman@protonmail.com>
2022-06-06 10:13:19 -0300

    Header and Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <nav>
+            <ul>
+                <li>
+                    <a href="https://github.com/matiassemelman" rel="noopener noreferrer" >GitHub</a>
+                </li>
+                <li>
+                    <a href="https://linkedin.com/matias-semelman" rel="noopener noreferrer" >Linkedin</a>
+                </li>
+            </ul>
+        </nav>
+    </footer>
+  )
+}
+
+export default Footer
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import { Link } from "react-router-dom"
 
 const Header = () => {
   return (
-    <h2>Header</h2>
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/list'>List</Link>
+          </li>
+          <li>
+            <Link to='/'>Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   )
 }
 
