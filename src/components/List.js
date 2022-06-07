881e560c9b988fa27e4805d81fb83afd819ff1e9
matiassemelman <matias.semelman@protonmail.com>
2022-06-07 14:02:23 -0300

    Added card

--- src/components/List.js
+++ src/components/List.js
@@ -1,19 +1,36 @@
 import { useNavigate } from "react-router";
-import {useEffect} from 'react'
+import { useEffect } from "react";
+import { Link } from 'react-router-dom';
 
 const List = () => {
   const navigate = useNavigate();
 
-
   useEffect(() => {
-    if (!localStorage.getItem('token')) {
-      navigate('/', {replace: true});
-   }
-  })
-  
-  return (
-    <>List</>
-  )
-}
+    if (!localStorage.getItem("token")) {
+      navigate("/", { replace: true });
+    }
+  });
 
-export default List
+  return (
+    <div className="row">
+      <div className="col-3 px-0" style={{ border: "1px solid red" }}>
+        <div class="card" style={{ width: "18rem" }}>
+          <img src="..." class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">Movie Title</h5>
+            <p class="card-text">
+              Review Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, dolorem.
+            </p>
+            <Link to="/" class="btn btn-primary">
+              View details
+            </Link>
+          </div>
+        </div>
+      </div>
+      
+     
+    </div>
+  );
+};
+
+export default List;
