bd9998622cc77264cdb7b5da46bc60decd7acd0e
matiassemelman <matias.semelman@protonmail.com>
2022-06-07 14:02:54 -0300

    Layout - organising elements

--- src/App.js
+++ src/App.js
@@ -5,21 +5,22 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Login from "./components/Login";
 import List from "./components/List";
-import Footer from "./components/Footer";
 
 // Styles
 import './css/bootstrap.min.css'
 
 function App() {
   return (
-    <>
+    <div class="container-fluid px-0">
       <Header />
+      <div class="container mt-3">
+        
       <Routes>
         <Route exact path="/" element={<Login />} />
         <Route path="/list" element={<List />} />
       </Routes>
-      <Footer/>
-    </>
+      </div>
+    </div>
   );
 }
 
