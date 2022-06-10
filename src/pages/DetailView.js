f2e0063fc0dde250bc2d0855c00ecdb3fe2ff91b
matiassemelman <matias.semelman@protonmail.com>
2022-06-10 14:48:07 -0300

    useEffect added. If !token => redirect to Login page

--- src/pages/DetailView.js
+++ src/pages/DetailView.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import { useNavigate } from "react-router-dom";
+import {useEffect} from "react"
+
+
 
 const DetailView = () => {
-  return (
-    <h2>DetailView</h2>
-  )
-}
+  const navigate = useNavigate();
+  let token = localStorage.getItem('token');
 
-export default DetailView
+  useEffect(() => {
+    !token && navigate("/", { replace: true });
+    }, [token]);
+    
+  return (
+    <>
+      
+      <h2>DetailView </h2>
+    
+    </>
+  );
+};
+
+export default DetailView;
