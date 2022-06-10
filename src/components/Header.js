94523d612cb604b2eb17ee1e7313ed8e08493041
matiassemelman <matias.semelman@protonmail.com>
2022-06-10 12:30:06 -0300

    Links fixed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 
 const Header = () => {
+  const token = localStorage.getItem('token')
   return (
     <header >
       <nav className="navbar navbar-dark bg-dark " role="navigation">
@@ -8,10 +9,10 @@ const Header = () => {
         <ul className="list-inline ms-5 ">
           <li className="list-inline-item text-light align-middle"><h4 >AlkeChallenge</h4></li>
           <li className="list-inline-item align-middle">
-            <Link to="/" className="text-light text-decoration-none">Home</Link>
+            <Link to={token ? "/list" : "/"} className="text-light text-decoration-none">Home</Link>
           </li>
           <li className="list-inline-item align-middle">
-            <Link to="/" className="text-light text-decoration-none">List</Link>
+            <Link to={token ? "/list" : "/"} className="text-light text-decoration-none">List</Link>
           </li>
         </ul>
       </nav>
